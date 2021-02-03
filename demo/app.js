1f269900ca97107109657771ff76a341187f3e86
Sheena O'Connell <sheena.oconnell@gmail.com>
2021-02-03 09:07:23 +0200

    gdg demo

--- demo/app.js
+++ demo/app.js
@@ -5,17 +5,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello JoziJs! [v1]").end();
+    res.status(200).send("Hello Google Dev Group! [Foo. v2]").end();
 });
 
 app.get("/oh-no", (req, res) => {
-  this;
-  is;
-  an;
-  error;
+    throw new Error("Golly, this was unexpected!")
 });
 
+app.get("/oh-no/2", (req, res) => {
+    bugger
+});
+  
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log("Press Ctrl+C to quit.");
